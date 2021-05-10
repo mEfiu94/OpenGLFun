3986dec38ea8b7d0f5d5bfc19c4f3e2690d266d9
mEfiu94 <mateusz.krajnik@protonmail.com.pl>
2021-05-10 20:51:57 +0200

    First inline shader for triangle written.

--- OpenGLFun/Source.cpp
+++ OpenGLFun/Source.cpp
@@ -2,6 +2,48 @@
 #include <GLFW/glfw3.h>
 #include "Window.h"
 #include <iostream>
+#include <string>
+
+static unsigned int CompileShader(const std::string& source, unsigned int type)
+{
+	auto id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if(result==GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length*sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << message << std::endl;
+		glDeleteShader(id);
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string & vertexShader, const std::string& fragmentShader) 
+{
+	auto program=glCreateProgram();
+	auto vs = CompileShader(vertexShader,GL_VERTEX_SHADER);
+	auto fs = CompileShader(fragmentShader, GL_FRAGMENT_SHADER);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+
+	glLinkProgram(program);
+	glValidateProgram(program);
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+
 
 int main(void)
 {
@@ -16,18 +58,36 @@ int main(void)
 
 
 	unsigned int bufferId;
-	constexpr float positions[8] = {
-		-0.5f, -0.5f,
-		0.0f, 0.5f,
-		0.5f, -0.5f,	
+	constexpr float positions[] = {
+		-0.1f, -0.1f,
+		0.0f, 0.1f,
+		0.1f, -0.1f,	
 	};
 	glGenBuffers(1, &bufferId);
 	glBindBuffer(GL_ARRAY_BUFFER, bufferId);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0,2,GL_FLOAT,false,2*sizeof(float),static_cast<const void*>(0));
+	std::string vertexShader =
+		R"(#version 330 core
 
+			layout(location=0) in vec4 position;
+			void main()
+			{
+				gl_Position=position;				
+			}
+)";
+	std::string fragmentShader =
+		R"(#version 330 core
 
+			layout(location=0) out vec4 color;
+			void main()
+			{
+				color=vec4(1.0,0.4,0.0,1.0);				
+			}
+)";
+	unsigned int shader = CreateShader(vertexShader,fragmentShader);
+	glUseProgram(shader);
 
 	while (!win.WindowShouldClose())
 	{
